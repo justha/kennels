47b92a85c78f0087d32f820c8278818b178b250d
justha <nganha42@gmail.com>
2020-09-08 17:04:54 -0500

    employee names how hyperlinks that dynamically route to employee details

--- src/components/employee/EmployeeList.js
+++ src/components/employee/EmployeeList.js
@@ -1,6 +1,7 @@
 import React, { useContext, useEffect } from "react"
 import { EmployeeContext } from "./EmployeeProvider"
 import { LocationContext } from "../location/LocationProvider"
+import { Link } from "react-router-dom"
 import "./Employee.css"
 // import { Employee } from "./EmployeeConverter"
 
@@ -22,63 +23,12 @@ export const EmployeeList = (props) => {
             <article className="employees">
                 {
                     employees.map(employee => {
-                        const employeeLocation = locations.find(loc => loc.id === employee.locationId) || {}
-                        return <section key={employee.id} className="employee">
+                        return <Link key={employee.id} className="employee" to={`/employees/${employee.id}`}>
                             <div><h3>{employee.name}</h3></div>
-                            <div>{employeeLocation.name}</div>
-                        </section>
+                        </Link>
                     })
                 }
             </article>
         </>
     )
-
-    // return (
-    //     <>
-    //         <div className="employees">
-    //             <h1>Employees</h1>
-    //             <button onClick={() => props.history.push("/employees/create")}>
-    //                 Add Employee
-    //             </button>
-    //             <article className="employeeList">
-    //                 {
-    //                     employees.map(employee => {
-    //                         const employeeLocation = locations.find(loc => loc.id === employee.locationId)
-    //                         return <section key={employee.id} className="employee" >
-    //                             <div><h3>Name: {employee.name}</h3></div>
-    //                             <div><h3>Location: {employeeLocation.name}</h3></div>
-    //                         </section>
-    //                     })
-    //                 }
-    //             </article>
-    //         </div>
-    //     </>
-    // )
-    
 }
-
-
-// return (
-//     <div className="employees">
-//         <h1>Employees</h1>
-//         <button onClick={() => props.history.push("/employees/create")}>
-//             Add Employee
-//         </button>
-//         <article className="employeeList">
-//             {employees.map(employee => <Employee key={employee.id} employee={employee} />)}
-//         </article>
-//     </div>
-// )
-
-// return (
-    //     <div className="employees">
-    //     {employees.map(emp => <Employee key={emp.id} employee={emp} />)}
-    //     </div>
-    // )
-    
-    
-    // Not needed; for instruction purposes only
-    // useEffect(() => {
-    //     console.log("EmployeeList: Employee state changed")
-    //     console.log(employees)
-    // }, [employees])
