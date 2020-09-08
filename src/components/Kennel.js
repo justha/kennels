8496456bdb5fd34895dbdbb524d3a210260fbe97
justha <nganha42@gmail.com>
2020-09-08 14:03:33 -0500

    user presented with login or register page

--- src/components/Kennel.js
+++ src/components/Kennel.js
@@ -1,57 +1,27 @@
 import React from "react"
-import { Route } from "react-router-dom"
+import { Route, Redirect } from "react-router-dom"
+import { ApplicationViews } from "./ApplicationViews"
 import { NavBar } from "./nav/NavBar"
-import { ApplicationViews } from "./ApplicationView";
+import { Login } from "./auth/Login"
+import { Register } from "./auth/Register"
 import "./Kennel.css"
-
 
 export const Kennel = () => (
     <>
-        <NavBar />
-        <ApplicationViews />
+        <Route render={() => {
+            if (localStorage.getItem("kennel_customer")) {
+                return (
+                    <>
+                        <Route render={props => <NavBar {...props} />} />
+                        <Route render={props => <ApplicationViews {...props} />} />
+                    </>
+                )
+            } else {
+                return <Redirect to="/login" />
+            }
+        }} />
+
+        <Route path="/login" render={props => <Login {...props} />} />
+        <Route path="/register" render={props => <Register {...props} />} />
     </>
 )
-
-
-// import { AnimalList } from "./animal/AnimalList"
-// import { AnimalProvider } from "./animal/AnimalProvider"
-// import { EmployeeList } from "./employee/EmployeeList";
-// import { EmployeeProvider } from "./employee/EmployeeProvider";
-// import { CustomerList } from "./customer/CustomerList"
-// import { CustomerProvider } from "./customer/CustomerProvider"
-// import { LocationList } from "./location/LocationList"
-// import { LocationProvider } from "./location/LocationProvider"
-
-
-// export const Kennel = () => (
-//     <>
-//         <h2>Nashville Kennels</h2>
-//         <small>Loving care when you're not there.</small>
-
-//         <address>
-//             <div>Visit Us at the Nashville North Location</div>
-//             <div>500 Puppy Way</div>
-//         </address>
-
-//         <h2>Animals</h2>
-//         <AnimalProvider>
-//             <AnimalList />
-//         </AnimalProvider>
-
-//         <h2>Employees</h2>
-//         <EmployeeProvider>
-//             <EmployeeList />
-//         </EmployeeProvider>
-     
-//         <h2>Locations</h2>
-//         <LocationProvider>
-//             <LocationList />
-//         </LocationProvider>
-
-//         <h2>Customers</h2>
-//         <CustomerProvider>
-//             <CustomerList />
-//         </CustomerProvider>
-        
-//     </>
-// )
